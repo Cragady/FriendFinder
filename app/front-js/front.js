0af6952465fd8273760f238b77c1f8b00a2e94c0
Cragady <pinkdotdreamer@gmail.com>
2018-07-19 03:21:30 -0600

    got radio buttons working; disabling and re-enabling submit button working

--- app/front-js/front.js
+++ app/front-js/front.js
@@ -8,7 +8,8 @@ var questions = ["Rock-climbing?",
     "Debating?",
     "Relaxing by doing nothing with others?",
     "Always being on the move, carrying out activities with others?"
-]
+];
+var friendNums = [];
 
 if($("#questionairre")){
     $("#questionairre").empty();
@@ -16,18 +17,19 @@ if($("#questionairre")){
     $.each(questions, function(i){
         $("#questionairre").append(`<section class="card bg-light my-5 py-2 text-center">
         <h4>${questions[i]}</h4>
-        <div id="buttons${i}" class="btn-group">`
+        <div id="buttons${i}" class="btn-group form-check form-check-inline">`
         );
             for(var dot = 0; dot < 10; dot++){
                 $(`#buttons${i}`).append(`<div class="mx-auto form-inline">
-                <input class="btn" type="radio" name="inlineRadioOptions" id="qArr${i}-pos${dot + 1}" value="${dot + 1}">
-                <label class="form-check-label" for="qArr${i}-pos${dot + 1}">${dot + 1}</label>
+                <input class="btn btn-choices-in form-check-input" type="radio" name="ansRadio${i}" id="qArr${i}-pos${dot + 1}" value="${dot + 1}">
+                <label class="radio-inline" for="qArr${i}-pos${dot + 1}">${dot + 1}</label>
                 </div>`
                 );
             };
         $("#questionairre").append(`</div></section>`);
         if(i === questions.length - 1){
-            $("#questionairre").append(`<button class="btn btn-primary">Submit</button>`)
+            $("#questionairre").append(`<button id="switch-me" class="btn btn-primary">Submit</button>`);
+            $("#switch-me").prop("disabled", true);
         };
     });
 };
@@ -39,5 +41,18 @@ $("#tester-bb").click(function(){
             $("#friendo-show-o").append("<h1>" + data[0][prop] + "</h1>");
         };
     });
+});
+
+$(".btn-choices-in").click(function(){
+    $.each(questions, function(i){
+        var numPush = $(`input[name=ansRadio${i}]:checked`).val();
+        friendNums[i] = numPush;
+    });
+    for(var i = 0; i < 10; i ++){
+        if(friendNums[i] === undefined){
+            return;
+        };
+    };
+    $("button#switch-me").prop("disabled", false);
 });
 
