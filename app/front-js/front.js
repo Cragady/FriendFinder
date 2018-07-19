2b4ef92a416438e522f12dfbe623b69373299dcc
Cragady <pinkdotdreamer@gmail.com>
2018-07-19 01:00:54 -0600

    added survey portion; in process of getting buttons situated

--- app/front-js/front.js
+++ app/front-js/front.js
@@ -12,15 +12,24 @@ var questions = ["Rock-climbing?",
 
 if($("#questionairre")){
     $("#questionairre").empty();
+    $("#questionairre").append(`<h3 class="card-header">On a scale of 1-10, how much do you like . . .</h3>`)
     $.each(questions, function(i){
-        $("#questionairre").append(`<h1>${questions[i]}</h1>`);
-        for(var dot = 0; dot < 10; dot++){
-            $("#questionairre").append(`<div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-            <label class="form-check-label" for="inlineRadio1">${dot + 1}</label>
-          </div>`)
-        }
-    })
+        $("#questionairre").append(`<section class="card bg-light my-5 py-2 text-center">
+        <h4>${questions[i]}</h4>
+        <div id="buttons${i}" class="btn-group">`
+        );
+            for(var dot = 0; dot < 10; dot++){
+                $(`#buttons${i}`).append(`<div class="mx-auto form-inline">
+                <input class="btn" type="radio" name="inlineRadioOptions" id="qArr${i}-pos${dot + 1}" value="${dot + 1}">
+                <label class="form-check-label" for="qArr${i}-pos${dot + 1}">${dot + 1}</label>
+                </div>`
+                );
+            };
+        $("#questionairre").append(`</div></section>`);
+        if(i === questions.length - 1){
+            $("#questionairre").append(`<button class="btn btn-primary">Submit</button>`)
+        };
+    });
 };
 
 $("#tester-bb").click(function(){
@@ -30,13 +39,5 @@ $("#tester-bb").click(function(){
             $("#friendo-show-o").append("<h1>" + data[0][prop] + "</h1>");
         };
     });
-    // $.ajax({
-    //     url: "/api/friends",
-    //     method: "GET"
-    // }).then(data =>{
-    //     for(var prop in data[0]){
-    //         $("#friendo-show-o").append("<h1>" + data[0][prop] + "</h1>");
-    //     };
-    // });
 });
 
