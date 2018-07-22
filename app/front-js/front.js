157c38b5a3546da9352e26489158d54759c8e26a
Cragady <pinkdotdreamer@gmail.com>
2018-07-21 18:37:20 -0600

    cleaned up front.js

--- app/front-js/front.js
+++ app/front-js/front.js
@@ -34,42 +34,47 @@ var testObj = {
     ]
 }
 
-if($("#questionairre")){
-    $("#questionairre").empty();
-    $("#questionairre").append(`
-        <h2 class="card-header text-center">Who Are You?</h2>
-        <div class="row justify-content-center">
-            <input class="mx-1 my-3 col-5" id="first-nam" placeholder="first">
-            <input class="mx-1 my-3 col-5" id="second-nam" placeholder="last"   >
-        </div>
-        <h2 class="card-header text-center">Picture Link</h2>
-        <input class="mx-1 my-3" id="pic-link">
-        <h3 class="card-header">On a scale of 1-10, how much do you like . . .</h3>`)
-    $.each(questions, function(i){
-        $("#questionairre").append(`<section class="card bg-light my-5 py-2 text-center">
-        <h4>${questions[i]}</h4>
-        <div id="buttons${i}" class="btn-group form-check form-check-inline">`
-        );
-            for(var dot = 0; dot < 10; dot++){
-                $(`#buttons${i}`).append(`<div class="mx-auto form-inline">
-                <input class="btn btn-choices-in form-check-input" type="radio" name="ansRadio${i}" id="qArr${i}-pos${dot + 1}" value="${dot + 1}">
-                <label class="radio-inline" for="qArr${i}-pos${dot + 1}">${dot + 1}</label>
-                </div>`
-                );
+function pageWriterStarter(){
+    if($("#questionairre")){
+        $("#questionairre").empty();
+        $("#questionairre").append(`
+            <h2 class="card-header text-center">Who Are You?</h2>
+            <div class="row justify-content-center">
+                <input class="mx-1 my-3 col-5" id="first-nam" placeholder="first">
+                <input class="mx-1 my-3 col-5" id="second-nam" placeholder="last"   >
+            </div>
+            <h2 class="card-header text-center">Picture Link</h2>
+            <input class="mx-1 my-3" id="pic-link">
+            <h3 class="card-header">On a scale of 1-10, how much do you like . . .</h3>`)
+        $.each(questions, function(i){
+            $("#questionairre").append(`<section class="card bg-light my-5 py-2 text-center">
+            <h4>${questions[i]}</h4>
+            <div id="buttons${i}" class="btn-group form-check form-check-inline">`
+            );
+                for(var dot = 0; dot < 10; dot++){
+                    $(`#buttons${i}`).append(`<div class="mx-auto form-inline">
+                    <input class="btn btn-choices-in form-check-input" type="radio" name="ansRadio${i}" id="qArr${i}-pos${dot + 1}" value="${dot + 1}">
+                    <label class="radio-inline" for="qArr${i}-pos${dot + 1}">${dot + 1}</label>
+                    </div>`
+                    );
+                };
+            $("#questionairre").append(`</div></section>`);
+            if(i === questions.length - 1){
+                $("#questionairre").append(`<button type="button" class="btn btn-primary" id="tester-bb" data-toggle="modal" data-target="#friendFound">Submit</button>`);
+                $("#tester-bb").prop("disabled", true);
             };
-        $("#questionairre").append(`</div></section>`);
-        if(i === questions.length - 1){
-            $("#questionairre").append(`<button type="button" class="btn btn-primary" id="tester-bb" data-toggle="modal" data-target="#friendFound">Submit</button>`);
-            $("#tester-bb").prop("disabled", true);
-        };
-    });
+        });
+    };
+
+    radioClicker();
+    submitSurvey();
 };
 
 function submitSurvey(){
-    $("#tester-bb").click(function(){//
-        event.preventDefault();//
+    $("#tester-bb").click(function(){
+        event.preventDefault();
         responseWriter(radioSetter);
-    });//
+    });
 };
 
 function responseWriter(writing){
@@ -83,8 +88,6 @@ function responseWriter(writing){
 }
 
 
-// $("#tester-bb").prop("disabled", false);
-// submitSurvey(testObj);
 function radioSetter(){
     var friendPass = new friendIn(friendNums);
     return friendPass;
@@ -102,12 +105,7 @@ function radioClicker(){
             };
         };
         $("#tester-bb").prop("disabled", false);
-        //submitSurvey(friendPass);
     });
 };
 
-radioClicker();
-submitSurvey();
-// console.log(radioSetter());
-
-
+pageWriterStarter();
